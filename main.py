d0c45b59fd265a6b1c025824d5be64856828fc85
roxana-xdx <142057783+roxana-xdx@users.noreply.github.com>
2025-12-23 18:14:17 +0200

    added score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
 from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
@@ -27,6 +27,13 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     Shot.containers = (shots, updatable, drawable)
     
+    pygame.font.init()
+    arial_font_path = pygame.font.match_font("arial")
+    if arial_font_path:
+        font = pygame.font.Font(arial_font_path, 30)
+    else:
+        font = pygame.font.Font(None, 36)
+    score = 0
 
     while(True):
         log_state()
@@ -46,14 +53,26 @@ def main():
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game over!")
+                print(f"Score: {score}")
                 sys.exit()
 
             for shot in shots:
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
+
+                    if asteroid.radius == ASTEROID_MIN_RADIUS:
+                        score += 15
+                    elif asteroid.radius == ASTEROID_MAX_RADIUS:
+                        score += 5
+                    else:
+                        score += 10
+
                     asteroid.split()
                     shot.kill()
         
+        score_text = font.render(f"Score: {score}", True, "white")
+        screen.blit(score_text, (10, 10))
+
         pygame.display.flip()
 
         # limit framerate to 60 FPS
